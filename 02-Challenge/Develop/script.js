adf8b5319d1557395ff7c84905d969835d899f68
CristianM <mcris740@gmail.com>
2023-03-09 16:24:18 -0500

    var values

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -3,10 +3,15 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var number = [0,1,2,3,4,5,6,7,8,9];
+  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "{", "}", "~"]
+  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   console.log(number);
-  return "Generate Password";
 
+
+
+
+  return "Generate Password";
 }
 
 // Write password to the #password input
