95cbd3fd3f4334637168832e38bc331ab1b1cde4
CristianM <mcris740@gmail.com>
2023-03-09 16:30:03 -0500

    comments

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -6,7 +6,11 @@ function generatePassword() {
   var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "{", "}", "~"]
   var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  console.log(number);
+  // Prompt user for PW criteria for length btw 8 to 128
+  // PW 8-128 characters
+  // uppercase, lowercase, numbers, symbols
+  // input validation
+  // display PW on the textarea
 
 
 
